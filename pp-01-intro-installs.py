f1427ca6fe45644cd6fe36ff45b1a2473d0e3cee
Sean Blessing <snblessing@gmail.com>
2025-02-13 07:30:50 -0500

    init commit

---
+++ pp-01-intro-installs.py
@@ -0,0 +1,13 @@
+# p. 3
+# Python requires installation and an IDE (Integrated Development Environment)
+# Python can be installed here: https://www.python.org/downloads/
+# VS Code can be installed here: https://code.visualstudio.com/download
+
+# To confirm installation, type "python -V" at command line
+# The first time writing Python in VS Code you may have to setup the Python Interpreter.
+# This is typically easy - just follow the prompts!
+
+# It's a good idea to pick a folder to work from, on your hard drive, before coding.
+# C:\repos\python\python-primer would work for those wishing to push code to GitHub.
+
+
